d5ad58a7ae2c000e689e420cf6999fd8b924698b
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 17:41:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
-  reverse_array = words_array.reverse
   words_array = string.split(' ')
     words_array.each do |words_array|
-      return words_array.reverse
+    words_array.reverse
     end
-    words_array.join(' ')
+    return words_array.join(' ')
 end
